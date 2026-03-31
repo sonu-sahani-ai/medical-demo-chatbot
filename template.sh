45eeddaf8f07d9c5677e535901634e2047c07029
Sonu <sahanisonu476@gmail.com>
2026-03-31 11:31:28 +0530

    floder structure added

---
+++ template.sh
@@ -0,0 +1,19 @@
+# create a new directory
+
+mkdir -p src
+mkdir -p research
+
+
+#creating  file
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch requirements.txt
+touch app.py
+touch research/trials.ipynb
+
+
+echo "Directory structure created successfully"
